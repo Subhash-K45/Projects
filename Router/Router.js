0144606f734915cf0f64a0a351733ac529d05c8f
Subhash-K45 <subhashk4137@gmail.com>
2023-06-07 22:39:52 +0530

    Router js upated

--- Router/Router.js
+++ Router/Router.js
@@ -22,6 +22,18 @@ router.get('/Contemporary_Paintings',async function (req,res){
    }
 })
 
+router.get('/Contemporary_Paintings/:id',async function(req,res){
+  try{
+    const SelectedId=req.params
+    const result=await Contemporary_image_Table.find(SelectedId)
+    res.send(result)
+    console.log(result)
+  }catch(err){
+    res.send('error')
+    console.log(err)
+  }
+})
+
 router.get('/Sculptures',async function(req,res){
   try{
     const result=await Sculpture_image_table.find()
@@ -31,12 +43,53 @@ router.get('/Sculptures',async function(req,res){
   }
 })
 
+router.get('/Sculptures/:id',async function(req,res){
+  try{
+    const SelectedId=req.params
+    const result=await Sculpture_image_table.find(SelectedId)
+    res.send(result)
+    console.log(result)
+  }catch(err){
+    res.send('error')
+    console.log(err)
+  }
+})
+
 router.get('/Mixed_Image',async function(req,res){
   try{
     const result=await Mixed_Art.find()
     res.send(result)
   }catch(err){
       console.log(err)
+  }
+})
+
+router.get('/Mixed_Image/:id',async function(req,res){
+  try{
+    const SelectedId=req.params
+    const result=await Mixed_Art.find(SelectedId)
+    res.send(result)
+    console.log(result)
+  }catch(err){
+    res.send('error')
+    console.log(err)
+  }
+})
+
+
+router.get('/Artist',async function(req,res){
+  try{
+    const res1=await Contemporary_image_Table.find({})
+    const obj=res1.map(({artist,category})=>{return {artist,category}})
+    
+    const res2=await Mixed_Art.find({})
+    obj.push(res2.map(({artist,category})=>{return {artist,category}}))
+
+    const res3=await Sculpture_image_table.find({})
+    obj.push(res3.map(({createrName,category})=>{return {createrName,category}}))
+    res.send(obj)
+    }catch(err){
+    console.log(err)
   }
 })
 
@@ -59,17 +112,25 @@ router.post('/SignUp',async function(req,res){
 
 router.post('/login',async function(req,res){
   try{
-    const {email}=req.body
+    const {email,password}=req.body
     const check = await SignUp_User.findOne({email:email})
     if(check){
-      
+      if(check.password===password){
+        res.send('Login Sucessful')
+      }
+      else{
+        res.send('Incorrect Password')
+      }
     }
     else{
-
+      res.send('User Not Found')
     }
   }catch(err){
     console.log(err)
   }
 })
+
+
+
 
 module.exports=router
