e3216cf5396a31ccf8a6119b7460aff4fb9f7ec8
Subhash-K45 <subhashk4137@gmail.com>
2023-06-13 20:56:13 +0530

    updated cors

--- ArtGallery-Backend/index.js
+++ ArtGallery-Backend/index.js
@@ -3,9 +3,11 @@ const bodyParser = require("body-parser");
 const app = express();
 const router = require("./Router/Router");
 const PORT = 8080;
+const cors=require('cors')
 
 app.use(bodyParser.json());
 app.use(router);
+app.use(cors())
 
 app.listen(PORT, () => {
   console.log(`App is listening to ${PORT}`);
