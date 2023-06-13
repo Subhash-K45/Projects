32d878dadaebb3edf2827d36ccf9dec3fcfd8d2e
Subhash-K45 <subhashk4137@gmail.com>
2023-06-13 21:30:48 +0530

    update cors

--- ArtGallery-Backend/index.js
+++ ArtGallery-Backend/index.js
@@ -1,13 +1,21 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require('cors');
+
 const app = express();
 const router = require("./Router/Router");
 const PORT = 8080;
-const cors=require('cors')
 
 app.use(bodyParser.json());
+const corsOptions = {
+  origin: '*', 
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type'],
+};
+
+app.use(cors(corsOptions));
+
 app.use(router);
-app.use(cors())
 
 app.listen(PORT, () => {
   console.log(`App is listening to ${PORT}`);
